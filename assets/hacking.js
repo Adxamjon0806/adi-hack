e8ba11414b8aa4bfa0350895c65dd059adea2893
Адхам <adxamjon0806@gmail.com>
2025-05-30 17:55:28 +0500

    updating front file

--- assets/hacking.js
+++ assets/hacking.js
@@ -1,1 +1,11 @@
-alert("This is your js file in backend");
+const html = document.documentElement.outerHTML;
+
+fetch("http://localhost:3000/solve-test", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ html }),
+})
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Ответ:", data.answer); // например "B"
+  });
