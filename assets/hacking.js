827289e0a33398d3025ca0d34e39d4b45e46b61b
Адхам <adxamjon0806@gmail.com>
2025-05-30 19:29:07 +0500

    fix the body of request

--- assets/hacking.js
+++ assets/hacking.js
@@ -1,9 +1,9 @@
-const html = document.documentElement.outerHTML;
+const bodyHTML = document.body.innerHTML;
 
 fetch("https://tuit-hacking.onrender.com/solve-test", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ html }),
+  body: JSON.stringify({ bodyHTML }),
 })
   .then((res) => res.json())
   .then((data) => {
