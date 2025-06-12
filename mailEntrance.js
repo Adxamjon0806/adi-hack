cea17b8c065ee0635488bea8037835b15a8f4e37
Адхам <adxamjon0806@gmail.com>
2025-06-12 13:40:29 +0500

    Fix 535 Authentication failed Error

--- mailEntrance.js
+++ mailEntrance.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  service: "gmail",
   auth: {
     user: process.env.GMAIL,
     pass: process.env.GOOGLE_MAIL_PASS,
