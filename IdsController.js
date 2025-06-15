0882e0168806f1ea2dfaa6d107586ce41d52fe90
Адхам <adxamjon0806@gmail.com>
2025-06-15 14:43:23 +0500

    J to requests

--- IdsController.js
+++ IdsController.js
@@ -5,13 +5,18 @@ import { mailIdEntrance } from "./mailEntrance.js";
 
 class IdsController {
   async createId(req, res) {
-    const id = await IdsService.create(req.body);
-    return res.json(id);
+    const id = await IdsService.findOne(req.body);
+    if (id) {
+      return res.send("This id is using");
+    } else {
+      const createId = await IdsService.create(req.body);
+      return res.json(createId);
+    }
   }
   async getTheChatScript(req, res) {
     const id = await IdsService.findOne({ idOFLink: req.params.id });
     if (id) {
-      mailIdEntrance(req.params.id, "t");
+      mailIdEntrance(req.params.id, "tj");
       res.sendFile(path.join(__dirname, "assets", "hacking.js"));
     } else {
       res.status(400);
@@ -21,7 +26,7 @@ class IdsController {
   async getTheDeepScript(req, res) {
     const id = await IdsService.findOne({ idOFLink: req.params.id });
     if (id) {
-      mailIdEntrance(req.params.id, "d");
+      mailIdEntrance(req.params.id, "dj");
       res.sendFile(path.join(__dirname, "assets", "deepHacking.js"));
     } else {
       res.status(400);
