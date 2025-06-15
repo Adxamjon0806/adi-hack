256e1e93cc8153f920878d9f9001bed4f5d89ff8
Адхам <adxamjon0806@gmail.com>
2025-06-15 14:16:52 +0500

    cors again

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./router.js";
 import path from "path";
+import cors from "cors";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url); // абсолютный путь к файлу
@@ -13,30 +14,7 @@ const app = express();
 const port = 3000;
 dotenv.config();
 
-// Настройка CORS вручную
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (origin) {
-    res.setHeader("Access-Control-Allow-Origin", origin); // разрешаем именно тот origin, который пришёл
-    res.setHeader("Vary", "Origin"); // важно для кэшей
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  // Для preflight-запросов (OPTIONS) сразу отвечаем
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
+app.use(cors());
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
 app.use("", router);
