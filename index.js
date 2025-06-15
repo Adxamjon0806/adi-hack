26ed78658b3e9836ce0a8f42119511d9bad2b6e3
Адхам <adxamjon0806@gmail.com>
2025-06-15 14:09:29 +0500

    Hand set cors

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import router from "./router.js";
@@ -14,10 +13,33 @@ const app = express();
 const port = 3000;
 dotenv.config();
 
+// Настройка CORS вручную
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin); // разрешаем именно тот origin, который пришёл
+    res.setHeader("Vary", "Origin"); // важно для кэшей
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Для preflight-запросов (OPTIONS) сразу отвечаем
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "5mb" }));
 app.use("", router);
-app.use(cors());
 
 const dbUri = process.env.MONGO_DB_KEY;
 
